249504ce363232e88c34d7022a10b9e32758e4d5
Gloire__ODG <93401301+ondongo@users.noreply.github.com>
2025-03-15 06:05:29 +0100

    Ajout d'un composant Suspense autour de ContainerVehicle pour améliorer la gestion du chargement
    - Intégration du composant Suspense dans la page des véhicules pour gérer l'état de chargement de ContainerVehicle.
    - Cette modification vise à améliorer l'expérience utilisateur en fournissant un retour visuel lors du chargement des données.

--- src/app/vehicles/page.tsx
+++ src/app/vehicles/page.tsx
@@ -2,7 +2,11 @@ import ContainerVehicle from "@/components/container/ContainerVehicle";
 import { Suspense } from "react";
 
 function page() {
-  return <ContainerVehicle />;
+  return (
+    <Suspense>
+      <ContainerVehicle />
+    </Suspense>
+  );
 }
 
 export default page;
