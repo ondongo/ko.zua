25e6935cac9accd8b3ff29760f5479e7d9a9c9ce
Gloire__ODG <93401301+ondongo@users.noreply.github.com>
2025-03-23 13:06:09 +0100

    Mise à jour des dépendances et refactorisation de la page NotFound
    - Ajout de nouvelles dépendances pour la visualisation des données, incluant `@react-jvectormap/core`, `@react-jvectormap/world`, `apexcharts`, et `react-apexcharts`.
    - Suppression du composant `CheckBox` qui n'est plus utilisé.
    - Refactorisation de la page `NotFound` pour améliorer l'interface utilisateur, ajout d'images et d'un lien de retour à la page d'accueil.
    - Mise à jour des permissions des fichiers pour assurer une exécution correcte.
    
    Ces changements améliorent la gestion des dépendances et enrichissent l'expérience utilisateur sur la page d'erreur 404.

---
+++ src/app/admin/(admin-pages)/page.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import React from "react";
+import DemographicCard from "@/components/metrics/DemographicCard";
+import MonthlySalesChart from "@/components/metrics/MonthlySalesChart";
+import MonthlyTarget from "@/components/metrics/MonthlyTarget";
+import { SimpleMetrics } from "@/components/metrics/SimpleMetrics";
+import StatisticsChart from "@/components/metrics/StatisticsChart";
+import RecentReservations from "@/components/metrics/RecentReservations";
+
+export const metadata: Metadata = {
+  title:
+    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
+  description: "This is Next.js Home for TailAdmin Dashboard Template",
+};
+
+export default function Ecommerce() {
+  return (
+    <div className="grid grid-cols-12 gap-4 md:gap-6">
+      <div className="col-span-12 space-y-6 xl:col-span-7">
+        <SimpleMetrics />
+
+        <MonthlySalesChart />
+      </div>
+
+      <div className="col-span-12 xl:col-span-5">
+        <MonthlyTarget />
+      </div>
+
+      <div className="col-span-12">
+        <StatisticsChart />
+      </div>
+
+      <div className="col-span-12 xl:col-span-5">
+        <DemographicCard />
+      </div>
+
+      <div className="col-span-12 xl:col-span-7">
+        <RecentReservations />
+      </div>
+    </div>
+  );
+}
--- src/app/admin/(full-width-pages)/(error-pages)/error-404/page.tsx
+++ src/app/admin/(full-width-pages)/(error-pages)/error-404/page.tsx
@@ -1,15 +1,22 @@
 import GridShape from "@/components/common/GridShape";
+import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-export default function NotFound() {
+export const metadata: Metadata = {
+  title: "Next.js Error 404 | TailAdmin - Next.js Dashboard Template",
+  description:
+    "This is Next.js Error 404 page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
+};
+
+export default function Error404() {
   return (
     <div className="relative flex flex-col items-center justify-center min-h-screen p-6 overflow-hidden z-1">
       <GridShape />
       <div className="mx-auto w-full max-w-[242px] text-center sm:max-w-[472px]">
         <h1 className="mb-8 font-bold text-gray-800 text-title-md dark:text-white/90 xl:text-title-2xl">
-          ERREUR
+          ERROR
         </h1>
 
         <Image
@@ -28,19 +35,19 @@ export default function NotFound() {
         />
 
         <p className="mt-10 mb-6 text-base text-gray-700 dark:text-gray-400 sm:text-lg">
-          Nous n&apos;arrivons pas à trouver la page que vous recherchez!
+          We can’t seem to find the page you are looking for!
         </p>
 
         <Link
           href="/"
           className="inline-flex items-center justify-center rounded-lg border border-gray-300 bg-white px-5 py-3.5 text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
         >
-          Retour à la page d&apos;accueil
+          Back to Home Page
         </Link>
       </div>
       {/* <!-- Footer --> */}
       <p className="absolute text-sm text-center text-gray-500 -translate-x-1/2 bottom-6 left-1/2 dark:text-gray-400">
-        &copy; {new Date().getFullYear()} - Kozua
+        &copy; {new Date().getFullYear()} - TailAdmin
       </p>
     </div>
   );
