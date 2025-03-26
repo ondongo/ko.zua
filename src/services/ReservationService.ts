41d5aa64e8f6208a3227f5b6f83f5a5fc7c62964
Gloire__ODG <93401301+ondongo@users.noreply.github.com>
2025-03-26 23:16:56 +0100

    Ajout de nouvelles fonctionnalités pour la gestion des réservations et des ventes
    - Implémentation de la fonction `deleteReservation` dans `ReservationService` pour permettre la suppression de réservations.
    - Ajout de la fonction `getAllReservations` avec pagination dans `ReservationService`, permettant de récupérer les réservations par page.
    - Mise à jour de `SaleService` avec l'ajout de la fonction `deleteSale` pour la suppression de ventes.
    - Intégration de la fonction `getAllSales` avec pagination dans `SaleService`, facilitant la récupération des ventes.
    
    Ces modifications visent à améliorer la gestion des réservations et des ventes, tout en optimisant l'expérience utilisateur grâce à la pagination.

--- src/services/ReservationService.ts
+++ src/services/ReservationService.ts
@@ -1,4 +1,5 @@
 import { ReservationRepository } from "@/repositories/ReservationRepository";
+import { PaginatedResult } from "@/types/allType";
 import { Reservation } from "@prisma/client";
 
 export class ReservationService {
@@ -10,5 +11,15 @@ export class ReservationService {
 
   async createReservation(reservationData: Reservation): Promise<void> {
     return this.repository.save(reservationData);
+  }
+  async deleteReservation(id: string): Promise<void> {
+    return this.repository.delete(id);
+  }
+
+  async getAllReservations(
+    page: number,
+    pageSize: number
+  ): Promise<PaginatedResult<Reservation>> {
+    return this.repository.findAll(page, pageSize);
   }
 }
--- src/services/SaleService.ts
+++ src/services/SaleService.ts
@@ -1,15 +1,24 @@
 import { SaleRepository } from "@/repositories/SaleRepository";
+import { PaginatedResult } from "@/types/allType";
 import { Vente } from "@prisma/client";
 
 export class SaleService {
-    private repository: SaleRepository;
-  
-    constructor(repository: SaleRepository) {
-      this.repository = repository;
-    }
-  
-    async createSale(saleData: Vente): Promise<void> {
-      return this.repository.save(saleData);
-    }
+  private repository: SaleRepository;
+
+  constructor(repository: SaleRepository) {
+    this.repository = repository;
   }
-  
+
+  async createSale(saleData: Vente): Promise<void> {
+    return this.repository.save(saleData);
+  }
+  async deleteSale(id: string): Promise<void> {
+    return this.repository.delete(id);
+  }
+  async getAllReservations(
+    page: number,
+    pageSize: number
+  ): Promise<PaginatedResult<Vente>> {
+    return this.repository.findAll(page, pageSize);
+  }
+}
