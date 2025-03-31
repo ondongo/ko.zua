22c583bf8ff8a8fc80d2863d9e8310787a21ee46
Gloire__ODG <93401301+ondongo@users.noreply.github.com>
2025-03-31 14:20:20 +0200

    Correction du texte des liens dans le composant `Footer`, remplaçant "Politique de confidentialité" par "Confidentialité" pour une meilleure clarté. Aucune modification de style n'a été apportée.

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -5,19 +5,24 @@ function Footer() {
   return (
     <footer className="bg-[#FAFAFA] py-4">
       <div className="container mx-auto text-center">
-      <div className="mb-2">
-          <a href="/confidentialite" className="text-gray-600 text-sm hover:underline mx-2">
-            Politique de confidentialité
+        <div className="mb-2">
+          <a
+            href="/confidentialite"
+            className="text-gray-600 text-sm hover:underline mx-2"
+          >
+            Confidentialité
           </a>
           |
-          <a href="/private-policies" className="text-gray-600 text-sm hover:underline mx-2">
+          <a
+            href="/private-policies"
+            className="text-gray-600 text-sm hover:underline mx-2"
+          >
             Conditions d'utilisation
           </a>
         </div>
         <p className="text-gray-600 text-sm">
           &copy; {new Date().getFullYear()} KO.Zua. Tous droits réservés.
         </p>
-     
       </div>
     </footer>
   );
