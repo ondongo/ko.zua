4edbbf14f2399d0ddc4e7267bccf679a0b69a4ee
Gloire__ODG <93401301+ondongo@users.noreply.github.com>
2025-08-30 04:49:57 +0200

    Modification des scripts dans le composant layout.tsx pour intégrer des scripts de CookieYes avec des attributs de gestion des cookies. Remplacement des balises Script par des balises script avec des types et des données appropriés pour le chargement conditionnel.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,19 +84,24 @@ export default function RootLayout({
 
         <Script
           id="cookieyes"
-          type="text/javascript"
           src="https://cdn-cookieyes.com/client_data/82e22ee47e26c562c52bdae0/script.js"
+          strategy="beforeInteractive" // 👈 important pour bloquer GA/Botpress, etc.
         />
-        <Script
+
+        {/* Ces 2 scripts seront chargés par CookieYes quand la catégorie sera acceptée */}
+        <script
+          type="text/plain"
+          data-cookieyes="functional"
           src="https://cdn.botpress.cloud/webchat/v3.2/inject.js"
-          strategy="afterInteractive"
           defer
-        />
-        <Script
+        ></script>
+
+        <script
+          type="text/plain"
+          data-cookieyes="functional"
           src="https://files.bpcontent.cloud/2025/07/22/06/20250722061503-W4OIX3K8.js"
-          strategy="afterInteractive"
           defer
-        />
+        ></script>
       </body>
     </html>
   );
